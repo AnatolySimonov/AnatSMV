ac6f51a2abdf9c8184f9124ae70f2553a275a405
AnatolySimonov <89423688+AnatolySimonov@users.noreply.github.com>
2021-08-24 01:38:51 +0300

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -2,7 +2,7 @@ birth_year = int(input("Enter your birth year"))
 required_year = int(input("Enter required year"))
 MAX_BY = 2021
 MIN_BY = 1900
-MAX_RY = 2100
+MAX_RY = 2150
 MIN_RY = 2022
 if MAX_BY >= birth_year >= MIN_BY and MAX_RY >= required_year >= MIN_RY:
     age = required_year - birth_year
