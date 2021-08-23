5da919e70a5c455a858dc1cbc30c56a82674b18c
AnatolySimonov <89423688+AnatolySimonov@users.noreply.github.com>
2021-08-24 00:44:26 +0300

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,10 @@
+salary1 = int(input("Enter salary for person 1"))
+salary2 = int(input("Enter salary for person 2"))
+salary3 = int(input("Enter salary for person 3"))
+
+if salary1 >= 0 and salary2 >= 0 and salary3 >= 0:
+    avg_salary = (salary1 + salary2 + salary3) // 3
+    print ("Your average salary is", avg_salary, "RU")
+else:
+    print("Error. Enter correct information")
+
