3010532d9a3766ac159e67b3ec47df14795fc14d
AnatolySimonov <89423688+AnatolySimonov@users.noreply.github.com>
2021-09-06 03:25:05 +0300

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,7 @@
+string1 = 'Съешь еще этих мягких французских булок ДА выпей же чаю'
+print(string1.split()[2].upper())
+print(string1.split()[6].lower())
+print(string1.split()[7].lower()[2])
+
+for words in range(1):
+    print(string1.split())
---
+++ Ex2.py
@@ -0,0 +1,3 @@
+for multiplier1 in range(1, 10):
+    for multiplier2 in range(1, 10):
+        print("%d*%d=%d" % (multiplier1, multiplier2, multiplier1 * multiplier2))
