0156d61060ba3ecd04550291af8a43b3c716f496
AnatolySimonov <89423688+AnatolySimonov@users.noreply.github.com>
2021-10-09 22:11:45 +0300

    Update Ex3.py

--- ДЗ11/Ex3.py
+++ ДЗ11/Ex3.py
@@ -34,23 +34,23 @@ class Professor(Person):
         return f'Преподаватель: {self.name} {self.surname} читает курс {self.course_to_read}'
 
 
-Semen = Professor(
+semen = Professor(
     name='Семен',
     surname='Семенов',
     course_to_read='ООП'
 )
-Konstantin = Student(
+konstantin = Student(
     name='Константин',
     surname='Консерватор'
 )
-Ivan = Student(
+ivan = Student(
     name='Иван',
     surname='Иванов'
 )
-students_list = [Konstantin.student_to_str(), Ivan.student_to_str()]
-first_test = Semen.test_students(students_list)
-second_test = Semen.test_students(students_list)
-print(Semen.__str__())
-print(Ivan.printer(Semen))
+students_list = [konstantin.student_to_str(), ivan.student_to_str()]
+first_test = semen.test_students(students_list)
+second_test = semen.test_students(students_list)
+print(semen.__str__())
+print(ivan.printer(semen))
 print(dict(zip(students_list, first_test)))
 print(dict(zip(students_list, second_test)))
